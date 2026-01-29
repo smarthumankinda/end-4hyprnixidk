770c85226982a222ab0527652121601a8399f07f
ImAwesome <231027182+smarthumankinda@users.noreply.github.com>
2026-01-29 07:40:51 +0530

    ahh

--- modules/qs.nix
+++ modules/qs.nix
@@ -12,6 +12,34 @@
         syntax-highlighting
         kirigami.unwrapped
       ];
+
+    fontConfig = pkgs.makeFontsConf {
+      fontDirectories = with pkgs; [
+        material-symbols
+        (
+          pkgs.stdenvNoCC.mkDerivation {
+            pname = "google-sans-flex";
+            version = "unstable";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "end-4";
+              repo = "google-sans-flex";
+              rev = "main";
+              sha256 = "sha256-HMAS0L/Tsqyl1xI16cyIzg9LEb6Dyq91JY4wqFQV9Vs=";
+            };
+
+            dontBuild = true;
+
+            installPhase = ''
+              runHook preInstall
+              install -Dm644 *.ttf \
+                -t $out/share/fonts/truetype/illogical-impulse-google-sans-flex
+              runHook postInstall
+            '';
+          }
+        )
+      ];
+    };
   in {
     packages.shell = inputs.wrappers.lib.wrapPackage {
       inherit pkgs;
@@ -23,6 +51,7 @@
 
       env = {
         QML2_IMPORT_PATH = "${qmlPath}";
+        FONTCONFIG_FILE = "${fontConfig}";
       };
 
       flags = {
